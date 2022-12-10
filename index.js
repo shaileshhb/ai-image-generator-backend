6df631916d988d94c1b98c3ea8d0bd3787b91c84
shaileshhb <shaileshb.0720@gmail.com>
2022-12-10 21:14:48 +0530

    cors package added:

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
+const cors = require('cors');
 const dotenv = require("dotenv").config()
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+// app.use(express.urlencoded({extended: false}))
 
 app.use("/openai", require("./routes/openaiRoutes"))
 
